d8b3a242e7f78563501a39a3783bb006be7d1764
Dinesh IIINC <dina.raja.s@gmail.com>
2018-08-26 12:46:30 +0530

    Initial Commit

---
+++ BmiCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BmiCalc
+//
+//  Created by Dinesh IIINC on 26/08/18.
+//  Copyright © 2018 dinaraja.me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
