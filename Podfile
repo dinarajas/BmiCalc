5d620f66d6b711dd7c3650fd40e841247b6a1040
Dinesh IIINC <dina.raja.s@gmail.com>
2018-08-26 16:43:47 +0530

    Updated pods

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'BmiCalc' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  # Pods for BmiCalc
+
+  pod 'RxSwift', '= 4.0.0'
+  pod 'RxCocoa', :git => 'https://github.com/ReactiveX/RxSwift.git', :commit => 'a1e6be8'
+
+  target 'BmiCalcTests' do
+    inherit! :search_paths
+    # Pods for testing
+
+    pod 'RxBlocking', '= 4.0.0'
+    pod 'RxTest', '= 4.0.0'
+  end
+end
---
+++ Pods/RxCocoa/RxCocoa/iOS/UISegmentedControl+Rx.swift
@@ -0,0 +1,43 @@
+//
+//  UISegmentedControl+Rx.swift
+//  RxCocoa
+//
+//  Created by Carlos García on 8/7/15.
+//  Copyright © 2015 Krunoslav Zaher. All rights reserved.
+//
+
+#if os(iOS) || os(tvOS)
+
+import UIKit
+#if !RX_NO_MODULE
+import RxSwift
+#endif
+
+
+extension Reactive where Base: UISegmentedControl {
+    /// Reactive wrapper for `selectedSegmentIndex` property.
+    public var selectedSegmentIndex: ControlProperty<Int> {
+        return value
+    }
+    
+    /// Reactive wrapper for `selectedSegmentIndex` property.
+    public var value: ControlProperty<Int> {
+        return base.rx.controlPropertyWithDefaultEvents(
+            getter: { segmentedControl in
+                segmentedControl.selectedSegmentIndex
+            }, setter: { segmentedControl, value in
+                segmentedControl.selectedSegmentIndex = value
+            }
+        )
+    }
+
+    /// Reactive wrapper for `setEnabled(_:forSegmentAt:)`
+    public func enabled(forSegmentAt segmentAt: Int) -> Binder<Bool> {
+        return Binder(self.base) { (segmentedControl, segmentEnabled) -> () in
+            segmentedControl.setEnabled(segmentEnabled, forSegmentAt: segmentAt)
+        }
+    }
+    
+}
+
+#endif
