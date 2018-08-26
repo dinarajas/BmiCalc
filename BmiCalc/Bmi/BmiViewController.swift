9e199b61b403737411387b1ce99aee488581b84e
Dinesh IIINC <dina.raja.s@gmail.com>
2018-08-26 13:42:22 +0530

    BmiViewController, Added BMI basic designs

---
+++ BmiCalc/Bmi/BmiViewController.swift
@@ -0,0 +1,11 @@
+//
+//  BmiViewController.swift
+//  BmiCalc
+//
+//  Created by Dinesh IIINC on 26/08/18.
+//  Copyright © 2018 dinaraja.me. All rights reserved.
+//
+
+import UIKit
+
+class BmiViewController: UIViewController {}
